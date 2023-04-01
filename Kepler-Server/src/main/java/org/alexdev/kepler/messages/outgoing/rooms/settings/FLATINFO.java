6351253cffbba52c6d3498fdd107eb89a5a40956
Stargazerdevelopments <gabrielle@stargazer-developments.com>
2023-04-01 06:30:02 +0200

    Fixed some packets for v9

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/rooms/settings/FLATINFO.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/rooms/settings/FLATINFO.java
@@ -33,8 +33,8 @@ public class FLATINFO extends MessageComposer {
         response.writeBool(this.room.getData().showOwnerName());
         response.writeBool(this.room.getCategory().hasAllowTrading()); // Allow trading
         response.writeBool(this.room.getCategory() == null);
-        response.writeInt(this.room.getData().getVisitorsNow());
         response.writeInt(this.room.getData().getVisitorsMax());
+        response.writeInt(this.room.getData().getVisitorsNow());
     }
 
     @Override
